bf1ba0bf218061bd172d7960e2094b79523bafcb
Mouli Bheemaneti <mouli.b@narayanagroup.com>
2025-07-23 07:24:09 +0530

    mac: read a predefined file

--- tauri-app/layouts/default.vue
+++ tauri-app/layouts/default.vue
@@ -1,6 +1,10 @@
 <template>
     <main>
         <SideMenu />
+        <div>
+            <h2>File Content</h2>
+            <pre>{{ fileContent }}</pre>
+        </div>
         <NuxtPage />
     </main>
 </template>
@@ -8,6 +12,33 @@
 <script setup>
 // This layout will automatically handle page transitions
 // based on the configuration in nuxt.config.ts
+import { exists, BaseDirectory } from '@tauri-apps/plugin-fs';
+import { invoke } from '@tauri-apps/api/core';
+import { onMounted } from 'vue';
+// when using `"withGlobalTauri": true`, you may use
+// const { exists, BaseDirectory } = window.__TAURI__.fs;
+const fileContent = ref();
+
+// Check if the `$APPDATA/avatar.png` file exists
+onMounted(async () => {
+    const contents = await exists('dev.md', { baseDir: BaseDirectory.Download });
+    console.log('exists', contents);
+
+    const filePath = "/Users/bmouli/Downloads/dev.md"; // Change to your file path
+
+
+invoke('read_file_if_exists', { path: filePath })
+  .then((content) => {
+    // content is the file data if it exists
+    console.log("File content:", content);
+    fileContent.value = content;
+  })
+  .catch((error) => {
+    // error is the error message if file does not exist or cannot be read
+    console.error("Error:", error);
+    fileContent.value = "error";
+  });
+});
 </script>
 
 <style scoped>
--- tauri-app/src-tauri/src/lib.rs
+++ tauri-app/src-tauri/src/lib.rs
@@ -1,14 +1,37 @@
+use tauri_plugin_fs::FsExt;
+
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+#[tauri::command]
+fn read_file_if_exists(path: &str) -> Result<String, String> {
+    use std::fs;
+    use std::path::Path;
+
+    if Path::new(path).exists() {
+        fs::read_to_string(path).map_err(|e| e.to_string())
+    } else {
+        Err("File does not exist".to_string())
+    }
+}
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .setup(|app| {
+            // allowed the given directory
+            let scope = app.fs_scope();
+            let allowed = scope.allow_directory("/Users/bmouli/Downloads", true);
+            dbg!(allowed);
+
+            Ok(())
+        })
+        .invoke_handler(tauri::generate_handler![greet, read_file_if_exists])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
